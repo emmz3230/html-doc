9ee6976c50f4ae278176e1ae034bf12c1e859623
emmz3230 <emmz3230@gmail.com>
2024-03-23 00:18:11 +0100

    added netlify app

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   favicon: "img/HTML_FULL_FORM_1.png",
 
   // Set the production url of your site here
-  url: "https://app.netlify.com/sites/html-documentations",
+  url: "https://html-documentations.netlify.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
